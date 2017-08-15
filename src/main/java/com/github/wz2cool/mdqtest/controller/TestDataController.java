ac7f17149ac97b8f39574dc31f7b0f260f6b3175
wz2cool <wz2cool@live.cn>
2017-08-15 10:38:18 +0800

    rename package

--- src/main/java/com/github/wz2cool/mdqtest/controller/TestDataController.java
+++ src/main/java/com/github/wz2cool/mdqtest/controller/TestDataController.java
@@ -1,7 +1,13 @@
 package com.github.wz2cool.mdqtest.controller;
 
+import com.github.wz2cool.mdqtest.mapper.ProductDao;
+import com.github.wz2cool.mdqtest.model.entity.table.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * \* Created with IntelliJ IDEA.
@@ -16,5 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/data")
 public class TestDataController {
 
+    @Autowired
+    private ProductDao productDao;
+
+    @RequestMapping(value = "getProducts", method = RequestMethod.GET)
+    public List<Product> getProducts() {
+        return productDao.selectAll();
+    }
 
 }
---
+++ src/main/java/com/github/wz2cool/mdqtest/mapper/ProductDao.java
@@ -0,0 +1,16 @@
+package com.github.wz2cool.mdqtest.mapper;
+
+import com.github.wz2cool.dynamic.mybatis.mapper.DynamicQueryMapper;
+import com.github.wz2cool.mdqtest.model.entity.table.Product;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: Frank
+ * \* Date: 8/15/2017
+ * \* Time: 10:36 AM
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public interface ProductDao extends DynamicQueryMapper<Product> {
+}
